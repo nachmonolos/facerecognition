27d8ab895dc2fca0675033e9da2e0db58dad2ee1
nachmonolos <68893120+nachmonolos@users.noreply.github.com>
2020-12-01 14:50:18 +0800

    show state

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./FaceRecognition.css";
 
-const FaceRecognition = ({ imageURL, boxes }) => {
+const FaceRecognition = ({ imageURL, boxes, show }) => {
   return (
     <div className="center ma">
       <div className="absolute mt2">
@@ -13,29 +13,23 @@ const FaceRecognition = ({ imageURL, boxes }) => {
           height="auto"
           style={{ borderRadius: "10px" }}
         />
+
         {boxes.map((box, i) => {
           return (
-            <div
-              key={i}
-              className="bounding-box"
-              style={{
-                top: boxes[i].topRow,
-                right: boxes[i].rightCol,
-                bottom: boxes[i].bottomRow,
-                left: boxes[i].leftCol,
-              }}
-            ></div>
+            show && (
+              <div
+                key={i}
+                className="bounding-box"
+                style={{
+                  top: boxes[i].topRow,
+                  right: boxes[i].rightCol,
+                  bottom: boxes[i].bottomRow,
+                  left: boxes[i].leftCol,
+                }}
+              ></div>
+            )
           );
         })}
-        {/* <div
-          className="bounding-box"
-          style={{
-            top: box.topRow,
-            right: box.rightCol,
-            bottom: box.bottomRow,
-            left: box.leftCol,
-          }}
-        ></div> */}
       </div>
     </div>
   );
